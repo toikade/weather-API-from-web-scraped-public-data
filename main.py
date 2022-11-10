9f8a89d1ddec55c3ddf3a0d953eba0bc1641e0c9
toikade <toikadeaaron@gmail.com>
2022-11-10 21:06:03 +0100

    used panda library to parse and organise scraped data

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
+import pandas as pd
 
 page = requests.get("https://forecast.weather.gov/MapClick.php?lon=-80.2269744873047&lat=26.01918730099294#.Y2wE2ffMLIU")
 print(page.status_code)
@@ -18,7 +19,19 @@ temp = [i.select(".temp")[0].get_text() for i in forecast_list_items]
 short_descs = [i.select(".short-desc")[0].get_text() for i in forecast_list_items]
 
 
-print(periods)
-print(descs)
-print(temp)
-print(short_descs)
+# print(periods)
+# print(descs)
+# print(temp)
+# print(short_descs)
+
+weather = pd.DataFrame({
+    "period":periods,
+    "desc":descs,
+    "temp":temp,
+    "short_descs":short_descs
+})
+
+num_temp = weather['temp'].str.extract("(\d+)", expand=False)
+weather['num_temp'] = num_temp.astype('int')
+
+print(type(weather))
